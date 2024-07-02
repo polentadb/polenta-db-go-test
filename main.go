3fc4e184ad7eabea092b3df739fcb429c2506f2e
Paulo Marcio Brandi Rezende <paulo@pmbr.net>
2024-07-02 11:42:44 -0400

    tcp client

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	// Connect to the server
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer conn.Close()
+
+	// Send data to the server
+	data := []byte("Hello, Server!")
+	_, err = conn.Write(data)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Read and process data from the server
+	// ...
+}
