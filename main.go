264f527c4b6c9a32dd70df929aee40506e6314e4
Paulo Marcio Brandi Rezende <paulo@pmbr.net>
2024-07-10 19:40:04 -0400

    implement statements test cases

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ import (
 )
 
 func main() {
+	testCreateBag()
+	testInsertBag()
+	testSelectBag()
+}
+
+func testCreateBag() {
 	// Connect to the server
 	conn, err := net.Dial("tcp", "localhost:8080")
 	if err != nil {
@@ -15,7 +21,57 @@ func main() {
 	defer conn.Close()
 
 	// Send data to the server
-	data := []byte("Hello, Server!")
+	data := []byte("create bag person (name string, age int)")
+	_, err = conn.Write(data)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	response := make([]byte, 1024)
+	n, err := conn.Read(response)
+	if err != nil {
+		return
+	}
+	fmt.Println(string(response[:n]))
+}
+
+func testInsertBag() {
+	// Connect to the server
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer conn.Close()
+
+	// Send data to the server
+	data := []byte("insert into bag person (name, age) values (\"John\", 30)")
+	_, err = conn.Write(data)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	response := make([]byte, 1024)
+	n, err := conn.Read(response)
+	if err != nil {
+		return
+	}
+	fmt.Println(string(response[:n]))
+}
+
+func testSelectBag() {
+	// Connect to the server
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer conn.Close()
+
+	// Send data to the server
+	data := []byte("select * from person")
 	_, err = conn.Write(data)
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -29,6 +85,4 @@ func main() {
 	}
 	fmt.Println(string(response[:n]))
 
-	// Read and process data from the server
-	// ...
 }
