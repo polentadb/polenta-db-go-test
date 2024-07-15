fcc3fd5b9bfdeec869a1d32cc2d3902c2ca2e914
Paulo Marcio Brandi Rezende <paulo@pmbr.net>
2024-07-15 19:35:52 -0400

    clean up code

--- main.go
+++ main.go
@@ -14,55 +14,11 @@ func main() {
 func testCreateBag() {
 	result := testStatement("create bag person (name string, age int)")
 	fmt.Println(result)
-	// // Connect to the server
-	// conn, err := net.Dial("tcp", "localhost:8080")
-	// if err != nil {
-	// 	fmt.Println("Error:", err)
-	// 	return
-	// }
-	// defer conn.Close()
-
-	// // Send data to the server
-	// data := []byte("create bag person (name string, age int)")
-	// _, err = conn.Write(data)
-	// if err != nil {
-	// 	fmt.Println("Error:", err)
-	// 	return
-	// }
-
-	// response := make([]byte, 1024)
-	// n, err := conn.Read(response)
-	// if err != nil {
-	// 	return
-	// }
-	// fmt.Println(string(response[:n]))
 }
 
 func testInsertBag() {
 	result := testStatement("insert into bag person (name, age) values (\"John\", 30)")
 	fmt.Println(result)
-	// // Connect to the server
-	// conn, err := net.Dial("tcp", "localhost:8080")
-	// if err != nil {
-	// 	fmt.Println("Error:", err)
-	// 	return
-	// }
-	// defer conn.Close()
-
-	// // Send data to the server
-	// data := []byte("insert into bag person (name, age) values (\"John\", 30)")
-	// _, err = conn.Write(data)
-	// if err != nil {
-	// 	fmt.Println("Error:", err)
-	// 	return
-	// }
-
-	// response := make([]byte, 1024)
-	// n, err := conn.Read(response)
-	// if err != nil {
-	// 	return
-	// }
-	// fmt.Println(string(response[:n]))
 }
 
 func testSelectBag() {
@@ -78,29 +34,6 @@ func testSelectBag() {
 	for i := 0; i < len(statements); i++ {
 		fmt.Println(<-c)
 	}
-	//testStatement("select * from person")
-	// // Connect to the server
-	// conn, err := net.Dial("tcp", "localhost:8080")
-	// if err != nil {
-	// 	fmt.Println("Error:", err)
-	// 	return
-	// }
-	// defer conn.Close()
-
-	// // Send data to the server
-	// data := []byte("select * from person")
-	// _, err = conn.Write(data)
-	// if err != nil {
-	// 	fmt.Println("Error:", err)
-	// 	return
-	// }
-
-	// response := make([]byte, 1024)
-	// n, err := conn.Read(response)
-	// if err != nil {
-	// 	return
-	// }
-	// fmt.Println(string(response[:n]))
 }
 
 func testSelect(statement string, c chan string) {
